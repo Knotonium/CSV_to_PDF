5c8e78d568042c9db3014bc6036d775c5f6d51ee
Knotonium <nicole.forouhar@lorent-online.com>
2021-10-03 19:35:05 +0200

    test

--- klassenbuch3/src/test/java/Epoch_Converter.java
+++ klassenbuch3/src/test/java/Epoch_Converter.java
@@ -2,7 +2,7 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-//unix time stamp
+// unix time stamp
 public class Epoch_Converter {
     public static String convertEpoch(String epoch){
         LocalDate ddMMyyyy = LocalDate.ofInstant(Instant.ofEpochSecond(Long.parseLong(epoch)), ZoneId.systemDefault());
